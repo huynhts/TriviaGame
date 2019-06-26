53e0b4397fd1d7e8ea24b0e0ee0d83e8e714dd53
winTS <huynhts@gmail.com>
2019-06-25 22:01:05 -0700

    added answer screen between questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
 //add global timer variables:
 var clockRunning = false;
 var timer = 15;
-// var timeLeft;
+var breakTime = 3;
+var correctAnswer = true;
+var timeLeft;
 
 var clickStart = false;
 
@@ -10,7 +12,6 @@ var clickStart = false;
 var questNum = 0;
 var correct = 0;
 var incorrect = 0;
-var noUserChoice = true; //switch to false each time user selects answer
 
 
 //display start button for user to begin -- onClick call start function and page change to display question 1: 
@@ -29,7 +30,7 @@ function start () {
     if (!clockRunning) {
         timeLeft = setInterval(count, 1000);
         clockRunning = true;
-
+        count ();
     }
 }
 
@@ -40,30 +41,71 @@ function count () {
     $('#timeDisplay').html('Time Remaining ' + timer + ' Seconds!');
 
     //add boolean value if user does not click answer or timer goes to zero, switch display to answer screen with gif
-        if (timer === 0 || !noUserChoice){
-            reset();
+        if (timer === 0){
+            correctAnswer = false;
+            answerScreen();
         }
 }
 
 function reset () {
+    clearInterval(timeLeft);
     timer = 15;
     $('#timeDisplay').html('Time Remaining ' + timer + ' Seconds!');
     clockRunning = false;
 
+
     if (clickStart === true){
         clickStart = false;
+        questNum = 0;
+        correct = 0;
+        incorrect = 0;
         $('#gameButton').css('display', 'block');
+    }
+}
+
+function checkQuestNum () {
+    if (questNum === 7) {
+        console.log(questNum);
+        $('.optionsBox').css('display', 'none');
+        $('.answerBreak').css('display', 'none');
+        
+    } else {
+        display();
     }
 }
 
 //create function to display answer screen between questions and toggle reset after screen timer goes out (3 second display)
 
-// function answerScreen () {  
-// }
+function answerScreen () {  
+    clearInterval(timeLeft);
+    setTimeout(checkQuestNum, 3000);
+    $('#timeDisplay').css('display', 'none');
+    $('.optionsBox').css('display', 'none');
+    $('.answerBreak').css('display', 'block');
+    $('#answerGif').html('<img src="' + questionObj[questNum].pic +'">')
+    console.log(questNum);
 
+    
+    if (correctAnswer) {
+        $('#rwAnswer').html('Correct Answer!');
+        correct++;
+        questNum++;
+
+    } else {
+        $('#rwAnswer').html('WRONG! The right answer is ' + questionObj[questNum].trivA[0]);
+        incorrect++;
+        questNum++;
+    }
+
+}
 
 //create function and pass through question object to randomly display nswers based on array index:
 function display () {
+    reset();
+    start();
+    $('#timeDisplay').css('display', 'block');
+    $('.optionsBox').css('display', 'block');
+    $('.answerBreak').css('display', 'none');
     var displayQ = questionObj[questNum].trivQ;
     var randomInt = [0,1,2,3];
     var a1 =[];
@@ -111,68 +153,44 @@ function display () {
 
 
 
-//create onclick function to check answer array index against correct answer index set at 0 and use clearInterval to clear the clock and reset it OR reset timer?
+//create onclick function to check answer array index against correct answer index set at 0
 $('#op1').on('click', function() {
     if ($('#op1').text() === questionObj[questNum].trivA[0]){
-        correct++;
-        questNum++;
-        console.log(correct);
-        console.log(questNum);
-        display();
+        correctAnswer = true;
+        answerScreen();
     } else {
-        incorrect++;
-        questNum++;
-        console.log(correct);
-        console.log(questNum);
-        display();
+        correctAnswer = false;
+        answerScreen();
     }
 });
 
 $('#op2').on('click', function() {
     if ($('#op2').text() === questionObj[questNum].trivA[0]){
-        correct++;
-        questNum++;
-        console.log(correct);
-        console.log(questNum);
-        display();
+        correctAnswer = true;
+        answerScreen();
     } else {
-        incorrect++;
-        questNum++;
-        console.log(correct);
-        console.log(questNum);
-        display();
+        correctAnswer = false;
+        answerScreen();
     }
 });
 
 $('#op3').on('click', function() {
     if ($('#op3').text() === questionObj[questNum].trivA[0]){
-        correct++;
-        questNum++;
-        console.log(correct);
-        console.log(questNum);
-        display();
+        correctAnswer = true;
+        answerScreen();
     } else {
-        incorrect++;
-        questNum++;        timer = 15;
-        console.log(correct);
-        console.log(questNum);
-        display();
+        correctAnswer = false;
+        answerScreen();
     }
 });
 
 $('#op4').on('click', function() {
     if ($('#op4').text() === questionObj[questNum].trivA[0]){
-        correct++;
-        questNum++;
-        console.log(correct);
-        console.log(questNum);
-        display();
+        correctAnswer = true;
+        answerScreen();
     } else {
-        incorrect++;
-        questNum++;
-        console.log(correct);
-        console.log(questNum);
-        display();
+        correctAnswer = false;
+        answerScreen();
     }
 });
 
@@ -181,23 +199,39 @@ $('#op4').on('click', function() {
 var questionObj = [
     question1 = {
         trivQ: 'Who won the last NBA championship?',
-        trivA: ['Raptors', 'Blazers','Celtics','Warriors']
+        trivA: ['Raptors', 'Blazers','Celtics','Warriors'],
+        pic: "https://media.giphy.com/media/XG14qInv52c4ZKGRsU/giphy.gif"
     },
     question2 = {
         trivQ: 'Which player has the most championship rings?',
-        trivA: ['Bill Russell','Kobe Bryant','Tony Parker','Michael Jordan']
+        trivA: ['Bill Russell','Kobe Bryant','Tony Parker','Michael Jordan'],
+        pic: "https://media.giphy.com/media/uj8LIbo44sybC4MKUF/giphy.gif"
     },
     question3 = {
         trivQ: 'What is the record for the most 3-point shots made by a single player in a game?',
-        trivA: [14, 8, 12, 15]
+        trivA: ['14', '8', '12', '15'],
+        pic: "https://media.giphy.com/media/elCvzK7dpU3SeHachG/giphy.gif"
     },
     question4 = {
         trivQ: 'Which player has missed the most field goals in the span of their career?',
-        trivA: ['Kobe Bryant', 'Rick Barry', 'Tim Duncan', 'Reggie Miller']
+        trivA: ['Kobe Bryant', 'Rick Barry', 'Tim Duncan', 'Reggie Miller'],
+        pic: "https://media.giphy.com/media/NfiEWXSA1fVoQ/giphy.gif"
+    },
+    question5 = {
+        trivQ: '"Big Smooth" was the nickname of which 20th century NBA player?',
+        trivA: ['Sam Perkins', 'Glen Davis', 'Demarcus Cousins', 'Nikola Jokic'],
+        pic: "https://media.giphy.com/media/2sCo3M6CKDF84/giphy.gif"
+    },
+    question6 = {
+        trivQ: 'Which NBA team attempts the fewest 3-pointer?',
+        trivA: ['Spurs', 'Pelicans', 'Clippers', 'Nets'],
+        pic: "https://media.giphy.com/media/TTgdzuXtaN5pMJ94L3W/giphy.gif"
+    },
+    question7 = {
+        trivQ: 'Who leads the league in free-throw percentage (2018-2019)?',
+        trivA: ['Malcolm Brogdon', 'Stephen Curry', 'Damian Lillard', 'Klay Thompson'],
+        pic: "https://media.giphy.com/media/ehmxhHYx7Nuc1WMDP7/giphy.gif"
     }
-
-
-
 ];
 
 
